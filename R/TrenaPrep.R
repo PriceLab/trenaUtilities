e9490ebc448bb0dc0e51b233e711d8db0cce686d
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-06-21 14:07:44 -0700

    standard footprings and dhs open chromatin filters work

--- R/TrenaPrep.R
+++ R/TrenaPrep.R
@@ -4,17 +4,23 @@
                            chromosome="character",
                            chromStart="numeric",
                            chromEnd="numeric",
-                           regulatoryRegionSources="list")
+                           regulatoryRegionSources="list",
+                           targetGeneTSS="numeric")
                         )
 #------------------------------------------------------------------------------------------------------------------------
-setGeneric('addGraph',         signature='obj', function(obj, graph, modelNames=list()) standardGeneric ('addGraph'))
-setGeneric('httpAddGraph',     signature='obj', function(obj, graph) standardGeneric ('httpAddGraph'))
-setGeneric('loadStyle',        signature='obj', function(obj, filename) standardGeneric ('loadStyle'))
+setGeneric('getRegulatoryRegions',  signature='obj', function(obj, combine=FALSE) standardGeneric ('getRegulatoryRegions'))
+setGeneric('getRegulatoryTableColumnNames',  signature='obj', function(obj) standardGeneric ('getRegulatoryTableColumnNames'))
+setGeneric('getGeneModelTableColumnNames',  signature='obj', function(obj) standardGeneric ('getGeneModelTableColumnNames'))
+setGeneric('expandRegulatoryRegionsTableByTF', signature='obj', function(obj, tbl.reg) standardGeneric('expandRegulatoryRegionsTableByTF'))
 #------------------------------------------------------------------------------------------------------------------------
-TrenaPrep = function(targetGene, chromosome, chromStart, chromEnd, regulatoryRegionSources=list(), quiet=TRUE)
+# a temporary hack: some constants
+genome.db.uri <- "postgres://bddsrds.globusgenomics.org/hg38"   # has gtf and motifsgenes tables
+#------------------------------------------------------------------------------------------------------------------------
+TrenaPrep = function(targetGene, targetGeneTSS, chromosome, chromStart, chromEnd, regulatoryRegionSources=list(), quiet=TRUE)
 {
 
    obj <- .TrenaPrep(targetGene=targetGene,
+                     targetGeneTSS=targetGeneTSS,
                      chromosome=chromosome,
                      chromStart=chromStart,
                      chromEnd=chromEnd,
@@ -24,4 +30,108 @@ TrenaPrep = function(targetGene, chromosome, chromStart, chromEnd, regulatoryReg
 
 } # constructor
 #------------------------------------------------------------------------------------------------------------------------
+setMethod('getRegulatoryTableColumnNames', 'TrenaPrep',
 
+      function(obj){
+         c("chrom", "motifStart", "motifEnd", "motifName", "strand", "score", "length", "distance.from.tss", "id", "tf")
+         })
+
+#------------------------------------------------------------------------------------------------------------------------
+setMethod('getGeneModelTableColumnNames', 'TrenaPrep',
+
+      function(obj){
+         c("tf", "randomForest", "pearson", "spearman", "betaLasso", "pcaMax", "concordance")
+         })
+
+#------------------------------------------------------------------------------------------------------------------------
+.callFootprintFilter <- function(obj, source, chromosome, chromStart, chromEnd, targetGene, targetGeneTSS)
+{
+    chromLocString <- sprintf("%s:%d-%d", chromosome, chromStart, chromEnd)
+    fpFilter <- FootprintFilter(genome.db.uri, source,  geneCenteredSpec=list(), regionsSpec=chromLocString)
+    x.fp <- getCandidates(fpFilter)
+    tbl.fp <- x.fp$tbl
+    colnames(tbl.fp) <- c("chrom", "motifStart", "motifEnd", "motifName", "length", "strand", "score1", "score", "score3", "tf")
+    distance <- tbl.fp$motifStart - targetGeneTSS
+    direction <- rep("upstream", length(distance))
+    direction[which(distance < 0)] <- "downstream"
+    tbl.fp$distance.from.tss <- distance
+    tbl.fp$id <- sprintf("%s.fp.%s.%06d.%s", targetGene, direction, abs(distance), tbl.fp$motifName)
+    tbl.fp <- tbl.fp[, getRegulatoryTableColumnNames(obj)]
+
+    tbl.fp
+
+} # .callFootprintFilter
+#------------------------------------------------------------------------------------------------------------------------
+.callHumanDHSFilter <- function(obj, source, chromosome, chromStart, chromEnd, targetGene, targetGeneTSS)
+{
+   browser()
+
+    chromLocString <- sprintf("%s:%d-%d", chromosome, chromStart, chromEnd)
+    dhsFilter <- HumanDHSFilter(genome="hg38",
+                                encodeTableName="wgEncodeRegDnaseClustered",
+                                pwmMatchPercentageThreshold=85L,
+                                geneInfoDatabase.uri=genome.db.uri,
+                                geneCenteredSpec=list(),
+                                regionsSpec=chromLocString)
+
+    x.dhs <- getCandidates(dhsFilter)
+    tbl.dhs <- x.dhs$tbl
+    tbl.dhs$length <- nchar(tbl.dhs$match)
+    distance <- tbl.dhs$motifStart - targetGeneTSS
+    direction <- rep("upstream", length(distance))
+    direction[which(distance < 0)] <- "downstream"
+
+    colnames(tbl.dhs)[grep("motifRelativeScore", colnames(tbl.dhs))] <- "score"
+    colnames(tbl.dhs)[grep("tfs", colnames(tbl.dhs))] <- "tf"
+    tbl.dhs$distance.from.tss <- distance
+    tbl.dhs$id <- sprintf("%s.fp.%s.%06d.%s", targetGene, direction, abs(distance), tbl.dhs$motifName)
+
+    tbl.dhs <- tbl.dhs[, getRegulatoryTableColumnNames(obj)]
+
+    tbl.dhs
+
+} # .callHumanDHSFilter
+#------------------------------------------------------------------------------------------------------------------------
+setMethod('getRegulatoryRegions', 'TrenaPrep',
+
+      function(obj, combine=FALSE){
+         tbl.combined <- data.frame()
+         result <- list()
+         encodeDHS.source.index <- grep("encodeHumanDHS", sources)
+
+         if(length(encodeDHS.source.index)){
+            sources <- sources[-encodeDHS.source.index]
+            tbl.dhs <- .callHumanDHSFilter(obj, source, obj@chromosome, obj@chromStart, obj@chromEnd,
+                                           obj@targetGene, obj@targetGeneTSS)
+            if(combine)
+               tbl.combined <- rbind(tbl.combined, tbl.dhs)
+            } # if encode DSH source requested
+
+         for(source in obj@regulatoryRegionSources){
+            tbl.fp <- .callFootprintFilter(obj, source, obj@chromosome, obj@chromStart, obj@chromEnd,
+                                           obj@targetGene, obj@targetGeneTSS)
+            if(combine)
+               tbl.combined <- rbind(tbl.combined, tbl.fp)
+            result[[source]] <- tbl.fp
+            } # for source
+         if(combine)
+            result[["all"]] <- tbl.combined
+         result
+         }) # getRegulatoryRegions
+
+#------------------------------------------------------------------------------------------------------------------------
+setMethod('expandRegulatoryRegionsTableByTF', 'TrenaPrep',
+
+     function(obj, tbl.reg){
+        tbl.trimmed <- subset(tbl.reg, nchar(tf) != 0)
+        tfs.split <- strsplit(tbl.trimmed$tf, ";")
+        #length(tfs.split) # [1] 36929
+        counts <- unlist(lapply(tfs.split, length))
+        tfs.split.vec <- unlist(tfs.split)
+        tbl.expanded <- expandRows(tbl.trimmed, counts, count.is.col=FALSE, drop=FALSE)
+        checkEquals(length(tfs.split.vec), nrow(tbl.expanded))
+        tbl.expanded$tf <- tfs.split.vec
+        tbl.expanded
+        }) # expandRegulatoryRegionsTableByTF
+
+#------------------------------------------------------------------------------------------------------------------------
--- inst/unitTests/test_TrenaPrep.R
+++ inst/unitTests/test_TrenaPrep.R
@@ -4,6 +4,10 @@ library(RUnit)
 runTests <- function()
 {
    test_constructor()
+   test_canonicalTableColumnNames()
+   test_.callFootprintFilterAndTFexpander()
+   test_getRegulatoryRegions_oneFootprintSource()
+   test_getRegulatoryRegions_twoFootprintSources()
 
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
@@ -11,9 +15,144 @@ test_constructor <- function()
 {
    printf("--- test_constructor")
    aqp4.tss <- 26865884
-   prep <- TrenaPrep("AQP4", "chr18", aqp4.tss-100, aqp4.tss+100, regulatoryRegionSources=list())
+   prep <- TrenaPrep("AQP4", targetGeneTSS=aqp4.tss, "chr18", aqp4.tss-100, aqp4.tss+100, regulatoryRegionSources=list())
    checkTrue(is(prep, "TrenaPrep"))
 
 } # test_constructor
+#------------------------------------------------------------------------------------------------------------------------
+test_canonicalTableColumnNames <- function()
+{
+   printf("--- test_canonicalTableColumnNames")
+   aqp4.tss <- 26865884
+   prep <- TrenaPrep("AQP4", targetGeneTSS=aqp4.tss, "chr18", aqp4.tss-100, aqp4.tss+100, regulatoryRegionSources=list())
+   checkTrue(is(prep, "TrenaPrep"))
+   checkTrue(all(getRegulatoryTableColumnNames(prep) ==
+                 c("chrom", "motifStart", "motifEnd", "motifName", "strand", "score", "length", "distance.from.tss", "id", "tf")))
+   checkTrue(all(getGeneModelTableColumnNames(prep) ==
+                 c("tf", "randomForest", "pearson", "spearman", "betaLasso", "pcaMax", "concordance")))
+
+
+} # test_canonicalTableColumnNames
+#------------------------------------------------------------------------------------------------------------------------
+test_.callFootprintFilterAndTFexpander <- function()
+{
+   printf("--- test_.callFootprintFilterAndTFexpander")
+   targetGene <- "AQP4"
+   aqp4.tss <- 26865884
+   chromosome <- "chr18"
+   chromStart <- aqp4.tss - 100
+   chromEnd   <- aqp4.tss + 100
+   source <- "postgres://whovian/brain_hint_20"
+
+   prep <- TrenaPrep("AQP4", aqp4.tss, chromosome, chromStart, chromEnd, regulatoryRegionSources=list(source))
+
+   tbl.fp <- TrenaHelpers:::.callFootprintFilter(prep, source, chromosome, chromStart, chromEnd,
+                                                 targetGene, targetGeneTSS=aqp4.tss)
+   checkTrue(all(colnames(tbl.fp) == getRegulatoryTableColumnNames(prep)))
+   checkTrue(nrow(tbl.fp) > 8 & nrow(tbl.fp) <20)
+   tbl.fpExpanded <- expandRegulatoryRegionsTableByTF(prep, tbl.fp)
+   checkTrue(nrow(tbl.fpExpanded) > 100)
+   checkTrue(min(nchar(tbl.fpExpanded$tf)) > 0)
+   checkTrue(max(nchar(tbl.fpExpanded$tf)) < 20)
+   checkEquals(length(grep(";", tbl.fpExpanded$tf)), 0)
+   checkTrue(length(grep(";", tbl.fp$tf)) > 0)
+
+} # test_.callFootprintFilterAndTFexpander
+#------------------------------------------------------------------------------------------------------------------------
+test_.callHumanDHSFilterAndTFexpander <- function()
+{
+   printf("--- test_.callHumanDHSFilterAndTFexpander")
+   targetGene <- "AQP4"
+   aqp4.tss <- 26865884
+   chromosome <- "chr18"
+   chromStart <- aqp4.tss - 100
+   chromEnd   <- aqp4.tss + 100
+   source <- "encodeHumanDHS"
+
+   prep <- TrenaPrep("AQP4", aqp4.tss, chromosome, chromStart, chromEnd, regulatoryRegionSources=list(source))
+
+   tbl.fp <- TrenaHelpers:::.callHumanDHSFilter(prep, source, chromosome, chromStart, chromEnd,
+                                                targetGene, targetGeneTSS=aqp4.tss)
+
+   checkTrue(all(colnames(tbl.fp) == getRegulatoryTableColumnNames(prep)))
+   checkTrue(nrow(tbl.fp) > 10 & nrow(tbl.fp) < 30)
+   tbl.fpExpanded <- expandRegulatoryRegionsTableByTF(prep, tbl.fp)
+   checkTrue(nrow(tbl.fpExpanded) > 100)
+   checkTrue(min(nchar(tbl.fpExpanded$tf)) > 0)
+   checkTrue(max(nchar(tbl.fpExpanded$tf)) < 20)
+   checkEquals(length(grep(";", tbl.fpExpanded$tf)), 0)
+   checkTrue(length(grep(";", tbl.fp$tf)) > 0)
+
+} # test_.callFootprintFilterAndTFexpander
+#------------------------------------------------------------------------------------------------------------------------
+test_getRegulatoryRegions_oneFootprintSource <- function()
+{
+   printf("--- test_getRegulatoryRegions_oneFootprintSource")
+   aqp4.tss <- 26865884
+   sources <- list("postgres://whovian/brain_hint_20")
+   prep <- TrenaPrep("AQP4", aqp4.tss, "chr18", aqp4.tss-100, aqp4.tss+100, regulatoryRegionSources=sources)
+   x <- getRegulatoryRegions(prep)
+   checkTrue(is(x, "list"))
+   checkEquals(names(x), as.character(sources))
+   checkTrue(all(unlist(lapply(x, function(element) is(element, "data.frame")), use.names=FALSE)))
+
+   tbl.reg <- x[[sources[[1]]]]
+   checkTrue(all(colnames(tbl.reg) == getRegulatoryTableColumnNames(prep)))
+
+} # test_getRegulatoryRegions_oneFootprintSource
+#------------------------------------------------------------------------------------------------------------------------
+test_getRegulatoryRegions_twoFootprintSources <- function()
+{
+   printf("--- test_getRegulatoryRegions_twoFootprintSources")
+   aqp4.tss <- 26865884
+   sources <- list("postgres://whovian/brain_hint_20",
+                   "postgres://whovian/brain_wellington_16")
+   prep <- TrenaPrep("AQP4", aqp4.tss, "chr18", aqp4.tss-100, aqp4.tss+100, regulatoryRegionSources=sources)
+
+      # get, but don't combined, footprints from both sources
+   x <- getRegulatoryRegions(prep, combine=FALSE)  # the default
+   checkTrue(is(x, "list"))
+   checkEquals(length(x), 2)
+   checkEquals(names(x), as.character(sources))
+   checkTrue(all(unlist(lapply(x, function(element) is(element, "data.frame")), use.names=FALSE)))
+
+   tbl.reg <- x[[sources[[1]]]]
+   checkTrue(all(colnames(tbl.reg) == getRegulatoryTableColumnNames(prep)))
+
+      # get AND combine  footprints from both sources
+   x <- getRegulatoryRegions(prep, combine=TRUE)  # the default
+   checkTrue(is(x, "list"))
+   checkEquals(length(x), 3)
+   checkEquals(names(x), c(as.character(sources), "all"))
+   checkTrue(all(unlist(lapply(x, function(element) is(element, "data.frame")), use.names=FALSE)))
+
+       # the "all" table should have rowcount the sum of the other two
+   summary <- lapply(x, nrow)
+   checkEquals(summary[["postgres://whovian/brain_hint_20"]], 13)
+   checkEquals(summary[["postgres://whovian/brain_wellington_16"]], 11)
+   checkEquals(summary[["all"]], 24)
+
+   checkTrue(all(colnames(x[[1]]) == getRegulatoryTableColumnNames(prep)))
+   checkTrue(all(colnames(x[[2]]) == getRegulatoryTableColumnNames(prep)))
+   checkTrue(all(colnames(x[[3]]) == getRegulatoryTableColumnNames(prep)))
+
+} # test_getRegulatoryRegions_twoFootprintSources
+#------------------------------------------------------------------------------------------------------------------------
+test_getRegulatoryRegions_encodeDHS <- function()
+{
+   printf("--- test_getRegulatoryRegions_encodeDHS")
+
+   aqp4.tss <- 26865884
+   sources <- list("encodeDHS")
+   prep <- TrenaPrep("AQP4", aqp4.tss, "chr18", aqp4.tss-100, aqp4.tss+100, regulatoryRegionSources=sources)
+   x <- getRegulatoryRegions(prep)
+   checkTrue(is(x, "list"))
+   checkEquals(names(x), as.character(sources))
+   checkTrue(all(unlist(lapply(x, function(element) is(element, "data.frame")), use.names=FALSE)))
+
+   tbl.reg <- x[[sources[[1]]]]
+   checkTrue(all(colnames(tbl.reg) == getRegulatoryTableColumnNames(prep)))
+
+} # test_getRegulatoryRegions_encodeDHS
 #------------------------------------------------------------------------------------------------------------------------
 
